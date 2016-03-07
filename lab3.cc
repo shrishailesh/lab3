5a879c39612821b0c74aa82b8d89402f3c8808bb
Rohit Belapurkar <rohitrtb@gmail.com>
2016-03-07 17:54:10 -0500

    Rohit Changes #1

--- lab3.cc
+++ lab3.cc
@@ -1,3 +1,6 @@
 /*use this for lab3. Commit and push such that no conflicts are generated.
-In case of conflicts, please roll back and resolve conficts*/
+In case of conflicts, please roll back and resolve conficts.
+This is a sample commit #1.
+-Rohit Belapurkar
+*/
 
