53ac908e7b0af2121775dc9250786b2ceb515d59
Rohit Belapurkar <rohitrtb@gmail.com>
2016-03-08 17:16:59 -0500

    Commit #2

--- lab3.cc
+++ lab3.cc
@@ -1,8 +1,4 @@
 /*use this for lab3. Commit and push such that no conflicts are generated.
 In case of conflicts, please roll back and resolve conficts.
-This is a sample commit #1.
--Rohit Belapurkar
-This is a sample commit #2
--Rohit Belapurkar
 */
 
